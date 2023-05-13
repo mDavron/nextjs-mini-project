3256c2f2900b8035f552f314262baa4663d95008
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-13 22:41:42 +0500

    typescript

--- src/components/PostInfo.tsx
+++ src/components/PostInfo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Heading from "./Heading";
+import Heading from "../components/Heading";
 
 const PostInfo = ({ post }) => {
   const { title, body } = post || {};
--- src/pages/api/socials.ts
+++ src/pages/api/socials.ts
@@ -1,6 +1,4 @@
-// export default function handler(req, res) {
-//   res.status(200).json({ name: "John Doe" });
-// }
+
 import { socials } from "./data/socialsdata";
 
 export default function handler(req, res) {
